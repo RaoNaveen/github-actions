2a58a2dbd3d1cdfa7e5e936231a12ec2d832f310
Naveen Yadav <naveen.yadav@paytm.com>
2025-07-31 16:45:06 +0530

    [Initial Commit]

---
+++ src/test/java/com/github/actions_demo/ActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.actions_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
